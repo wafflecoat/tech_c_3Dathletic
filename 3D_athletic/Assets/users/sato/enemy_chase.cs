932e16a625a2f46061c810b232ce662ea4079bab
wafflecoat <TTC22C1020026@edu.tech.ac.jp>
2022-06-19 18:07:00 +0900

    鬼が追いかける1

--- 3D_athletic/Assets/users/sato/enemy_chase.cs
+++ 3D_athletic/Assets/users/sato/enemy_chase.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class enemy_chase : MonoBehaviour
 {
+    private GameObject target;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0.035f;
+        target = GameObject.FindGameObjectWithTag("Player");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //プレイヤーの方を向いてから前に進む
+        transform.LookAt(target.transform);
+        transform.position += transform.forward * speed;
     }
 }
