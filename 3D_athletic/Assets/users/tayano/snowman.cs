594b845b7c66b2eca13397b9119343d22b40f8a7
Ryo <aaaaaa>
2022-06-30 14:13:21 +0900

    雪だるま動きver,1.1

--- 3D_athletic/Assets/users/tayano/snowman.cs
+++ 3D_athletic/Assets/users/tayano/snowman.cs
@@ -15,7 +15,7 @@ public class snowman : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 p = new Vector3(0, 0, move);
+        Vector3 p = new Vector3(0,move,0);
         transform.Translate(p);
         counter++;
 
