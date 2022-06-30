252331926811b75794913be9b19436d5490b28e7
nakahari38 <ttc22c1020038@edu.tech.ac.jp>
2022-06-30 14:23:04 +0900

    効果音付き

--- 3D_athletic/Assets/users/nakahari/Player.cs
+++ 3D_athletic/Assets/users/nakahari/Player.cs
@@ -8,11 +8,14 @@ public class Player : MonoBehaviour
     private Rigidbody rb;
     private bool isJumping = false;
     private Vector3 latestPos;  //前回のPosition
+    public AudioClip jump12;
+    AudioSource audioSource;
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     [SerializeField] private Vector3 velocity; //移動方向
@@ -33,7 +36,6 @@ public class Player : MonoBehaviour
         if (Input.GetKey(KeyCode.A))
         {
             velocity.x -= 1;
-
         }
             
         //後ろへ進む
@@ -62,6 +64,7 @@ public class Player : MonoBehaviour
         {
             rb.velocity = Vector3.up * jumpPower;
             isJumping = true;
+            audioSource.PlayOneShot(jump12);
         }
 
         Vector3 diff = transform.position - latestPos;   //前回からどこに進んだかをベクトルで取得
@@ -72,8 +75,6 @@ public class Player : MonoBehaviour
         {
             transform.rotation = Quaternion.LookRotation(velocity); //向きを変更する
         }
-
-
     }
     private void OnCollisionEnter(Collision collision)
     {
