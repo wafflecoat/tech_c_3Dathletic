5c8d01d4e368ef82049fca1d5b78d0f3bbd126ea
sekkirtech <ttc22c1020033@edu.tech.ac.jp>
2022-07-05 21:32:37 +0900

    バグ修正
    titleSceneが読み込まれる度にBGMが重複するバグの修正

--- 3D_athletic/Assets/users/seki/gamebgm.cs
+++ 3D_athletic/Assets/users/seki/gamebgm.cs
@@ -1,23 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class gamebgm : MonoBehaviour
 {
+    public AudioClip BGM_title;
+
+    private AudioSource source;
+
+    private string beforeScene = "TitleScene";
+
     public bool DontDestroyEnabled = true;
     // Start is called before the first frame update
     void Start()
     {
+
         if (DontDestroyEnabled)
         {
       
             DontDestroyOnLoad(this);
         }
+
+        source = GetComponent<AudioSource> ();
+
+        source.clip = BGM_title;
+		source.Play();
+
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+    }
+
+
+    void OnActiveSceneChanged ( Scene prevScene, Scene nextScene ) {
+		
+		
+		if (beforeScene == "ClearScene" && nextScene.name == "TitleScene")
+        {
+			source.Stop ();
+		}
+
+		//メインからメニューへ
+		if (beforeScene == "OrverScene" && nextScene.name == "TitleScene") 
+        {
+			source.Stop ();
+		}
+        beforeScene = nextScene.name;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
+    
 }
