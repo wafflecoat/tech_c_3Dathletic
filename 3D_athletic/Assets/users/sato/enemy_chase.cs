5d977330b9221d2f26989b50697f521130ff3fce
wafflecoat <TTC22C1020026@edu.tech.ac.jp>
2022-07-05 18:26:57 +0900

    時使いバグ修正、音量調整

--- 3D_athletic/Assets/users/sato/enemy_chase.cs
+++ 3D_athletic/Assets/users/sato/enemy_chase.cs
@@ -5,34 +5,33 @@ using UnityEngine.SceneManagement;
 
 public class enemy_chase : MonoBehaviour
 {
-    private Rigidbody rb;
-    private GameObject target;
-    public float speed;
+    private Rigidbody rb;//Rigidbodyの情報
+    private GameObject target;//追いかける対象の情報
+    [SerializeField] private float speed = 1.0f;//速度
+    private Vector3 lookplayer;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        speed = 0.005f;
         target = GameObject.FindGameObjectWithTag("Player1");
     }
 
     // Update is called once per frame
     void Update()
     {
-        //プレイヤーの方を向いてから前に進む
-        transform.LookAt(target.transform);
-        transform.position += transform.forward * speed;
+        transform.LookAt(target.transform);//プレイヤーの方を向く
+        transform.position += transform.forward * speed * Time.deltaTime;//向いている方に進む
     }
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player1")
+        if (collision.gameObject.tag == "Player1")//鬼に捕まった時
         {
             SceneManager.LoadScene("OrverScene");
         }
-        rb.isKinematic = true;
+        rb.isKinematic = true;//障害物に当たったときに物理の処理をisKinematicに変更する
     }
     void OnCollisionExit(Collision collision)
     {
-        rb.isKinematic = false;
+        rb.isKinematic = false;//障害物から離れたときにisKinematicをはずす
     }
 }
